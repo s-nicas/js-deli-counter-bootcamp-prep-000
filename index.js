acd4d4f05028ab63f226e261158c60d73ac9e3f6
Sonia  D <sduarte22@gmail.com>
2018-03-02 21:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli=[];
 
 function takeANumber (katzDeli, newCustomer){
   katzDeli.push(newCustomer);
-  return katzDeli.length+1
+  return 'Welcome, ${katzDeli.length+1}. You are number 1 in line.'
   
 }
