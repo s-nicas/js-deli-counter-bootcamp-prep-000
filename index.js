15457264e99335e3f2757b949a3759b3d48011c3
Sonia  D <sduarte22@gmail.com>
2018-03-02 21:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeli=[];
 
 function takeANumber (katzDeli, newCustomer){
   katzDeli.push(newCustomer);
-  return 'Welcome, ' +newCustomer+ '. You are number ' +katzDeli.length+1+ ' in line.';
+  return 'Welcome, ' +newCustomer+ '. You are number ' +katzDeli.length + ' in line.';
   
 }
